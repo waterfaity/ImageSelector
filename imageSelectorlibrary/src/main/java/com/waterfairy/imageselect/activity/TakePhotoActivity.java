adf7d9c8e369e8536951dc5ca5fa314aef1a2d35
xucw <995637517@qq.com>
2019-12-16 10:14:55 +0800

    去掉 拍照时创建父文件夹

--- imageSelectorlibrary/src/main/java/com/waterfairy/imageselect/activity/TakePhotoActivity.java
+++ imageSelectorlibrary/src/main/java/com/waterfairy/imageselect/activity/TakePhotoActivity.java
@@ -37,10 +37,23 @@ public class TakePhotoActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.image_selector_activity_image_select_take_photo);
-        mImg = findViewById(R.id.img);
+        getExtra();
+        findView();
+        initData();
+    }
+
+    private void getExtra() {
         options = (TakePhotoOptions) getIntent().getSerializableExtra(ConstantUtils.OPTIONS_BEAN);
         compressOptions = (CompressOptions) getIntent().getSerializableExtra(ConstantUtils.OPTIONS_COMPRESS_BEAN);
         if (options == null) options = new TakePhotoOptions();
+    }
+
+    private void findView() {
+        mImg = findViewById(R.id.img);
+    }
+
+    private void initData() {
+
         Intent intent = new Intent();
         // 指定开启系统相机的Action
         intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -49,12 +62,23 @@ public class TakePhotoActivity extends BaseActivity {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
         String format = simpleDateFormat.format(new Date());
         file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + "/Camera", "IMG_" + format + ".jpg");
+        //创建父类文件夹
+        createParentFile();
         // 把文件地址转换成Uri格式
         ProviderUtils.setAuthority(options.getPathAuthority());
         Uri uri = ProviderUtils.getProviderUri(this, intent, file);
         // 设置系统相机拍摄照片完成后图片文件的存放地址
         intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
         startActivityForResult(intent, options.getRequestCode());
+    }
+
+    private void createParentFile() {
+        if (file != null) {
+            File parentFile = file.getParentFile();
+            if (parentFile != null && !parentFile.exists()) {
+                boolean mkdirs = parentFile.mkdirs();
+            }
+        }
     }
 
     @Override
